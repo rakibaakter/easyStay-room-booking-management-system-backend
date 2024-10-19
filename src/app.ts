ebe1a977f0cfa99ef6c80755526f754585aad59f
rakibaakter <rakibaakter635@gmail.com>
2024-10-20 01:02:17 +0600

    created cathcAsync utities for ignore try catch repeatition, sendResponse to specify the response, and global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from "cors"
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app : Application = express();
 
 // parser
@@ -13,5 +14,8 @@ app.use("/api/", router);
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello World!')
 })
+
+// global error handle
+app.use(globalErrorHandler as any);
 
 export default app;
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,17 +1,26 @@
-import { Request, Response } from "express";
-import { userServices } from "./user.service";
 
-const createUser = async(req : Request, res : Response )=>{
+
+import { userServices } from "./user.service";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+
+
+// call catchAsyc funtion for handle try catch in every controller
+const createUser = catchAsync(async(req, res )=>{
 
     const {user : userData} = req.body 
     const result = await userServices.createUserIntoDB(userData);
 
-    res.status(200).json({
+
+    // send response
+    sendResponse(res,{
+        statusCode : 200,
         success : true,
         message : "User created successfully!",
         data : result
-    })
-}
+    } )
+    
+})
 
 export const userControllers = {
     createUser
