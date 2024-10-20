6ecc09d1f1251c69d4c3c1748fceebf43650e8de
rakibaakter <rakibaakter635@gmail.com>
2024-10-21 00:52:46 +0600

    comment update

--- src/app/modules/room/room.service.ts
+++ src/app/modules/room/room.service.ts
@@ -1,21 +1,23 @@
 import { TRoom } from "./room.interface";
 import { Room } from "./room.model";
 
+// upload room into db
 const createRoomIntoDB = async(payload : TRoom)=>{
     const result = await Room.create(payload);
     return result
 }
 
+// get all room from db
 const getAllRoomFromDB = async()=>{
     const result = await Room.find();
     return result;
 }
 
+// get single room from db by id
 const getSingleRoomByIdFromDB = async(id : string)=>{
     const result = await Room.findById(id);
     return result;
 }
-
 
 export const roomServices = {
     createRoomIntoDB,
