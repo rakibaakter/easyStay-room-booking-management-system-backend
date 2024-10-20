37cdf2506c7457c8d609990356670a3e21a23b70
rakibaakter <rakibaakter635@gmail.com>
2024-10-20 17:50:18 +0600

    implemented delete api to room model

--- src/app/modules/room/room.controller.ts
+++ src/app/modules/room/room.controller.ts
@@ -2,48 +2,64 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { roomServices } from "./room.service";
 
-const createRoom = catchAsync(async(req, res)=>{
-    const {room : roomData} = req.body;
-    const result = await roomServices.createRoomIntoDB(roomData)
+// call catchAsyc funtion for handle try catch in every controller
 
-    // send response
-    sendResponse(res,{
-        statusCode : 200,
-        success : true,
-        message : "Room created successfully!",
-        data : result
-    } )
-})
+// for post api
+const createRoom = catchAsync(async (req, res) => {
+  const { room: roomData } = req.body;
+  const result = await roomServices.createRoomIntoDB(roomData);
 
-const getAllRoom = catchAsync(async(req, res)=>{
-    const result = await roomServices.getAllRoomFromDB()
+  // send response
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Room created successfully!",
+    data: result,
+  });
+});
 
-    // send response
-    sendResponse(res,{
-        statusCode : 200,
-        success : true,
-        message : "Rooms are retrived successfully!",
-        data : result
-    } )
-})
+// for get all api
+const getAllRoom = catchAsync(async (req, res) => {
+  const result = await roomServices.getAllRoomFromDB();
 
-const getRoomById = catchAsync(async(req, res)=>{
-    const result = await roomServices.getSingleRoomByIdFromDB(req.params.id)
+  // send response
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Rooms are retrived successfully!",
+    data: result,
+  });
+});
 
-    // send response
-    sendResponse(res,{
-        statusCode : 200,
-        success : true,
-        message : "Room retrived successfully!",
-        data : result
-    } )
-})
+// for single get api
+const getRoomById = catchAsync(async (req, res) => {
+  const result = await roomServices.getSingleRoomByIdFromDB(req.params.id);
 
+  // send response
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Room retrived successfully!",
+    data: result,
+  });
+});
 
+// for delete api
+const deletRoomById = catchAsync(async (req, res) => {
+  const result = await roomServices.deletSingleRoomByIdFromDB(req.params.id);
 
+  // send response
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Room deleted successfully!",
+    data: result,
+  });
+});
 
 export const roomControllers = {
-    createRoom,
-    getAllRoom,
-    getRoomById
-}
+  createRoom,
+  getAllRoom,
+  getRoomById,
+  deletRoomById,
+};
--- src/app/modules/room/room.service.ts
+++ src/app/modules/room/room.service.ts
@@ -1,24 +1,33 @@
 import { TRoom } from "./room.interface";
 import { Room } from "./room.model";
 
-const createRoomIntoDB = async(payload : TRoom)=>{
-    const result = await Room.create(payload);
-    return result
-}
+// post data to room collection into DB
+const createRoomIntoDB = async (payload: TRoom) => {
+  const result = await Room.create(payload);
+  return result;
+};
 
-const getAllRoomFromDB = async()=>{
-    const result = await Room.find();
-    return result;
-}
+// get all room from DB
+const getAllRoomFromDB = async () => {
+  const result = await Room.find();
+  return result;
+};
 
-const getSingleRoomByIdFromDB = async(id : string)=>{
-    const result = await Room.findById(id);
-    return result;
-}
+// get single room by its id
+const getSingleRoomByIdFromDB = async (id: string) => {
+  const result = await Room.findById(id);
+  return result;
+};
 
+// delete single room by its id
+const deletSingleRoomByIdFromDB = async (id: string) => {
+  const result = await Room.findByIdAndDelete(id);
+  return result;
+};
 
 export const roomServices = {
-    createRoomIntoDB,
-    getAllRoomFromDB,
-    getSingleRoomByIdFromDB
-}
+  createRoomIntoDB,
+  getAllRoomFromDB,
+  getSingleRoomByIdFromDB,
+  deletSingleRoomByIdFromDB,
+};
